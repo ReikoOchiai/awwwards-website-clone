b3ff03b426b0f3cc8ed0a1a2149a70990a2f9923
Reiko Ochiai <lissage.23@gmail.com>
2024-03-23 15:16:58 +1100

    🏷️  Types: update layout file to be tsx and update metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+	title: 'Tech Blob',
+	description:
+		'Articles about tech insights and journal of building SaaS Software',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	)
+}
